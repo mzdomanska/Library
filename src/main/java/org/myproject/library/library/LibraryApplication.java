e83730e2fc6be89d8b9d2cfc608c6648f0d5ed88
Magdalena Domanska <m.z.domanska@gmail.com>
2018-04-05 14:52:47 +0200

    Create HomePageController
    * home.html, home.css and HomePageController created

--- src/main/java/org/myproject/library/library/LibraryApplication.java
+++ src/main/java/org/myproject/library/library/LibraryApplication.java
@@ -1,7 +1,9 @@
 package org.myproject.library.library;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.util.UUID;
 
---
+++ src/test/java/org/myproject/library/library/ControllerTest/HomeControllerTest.java
@@ -0,0 +1,20 @@
+package org.myproject.library.library.ControllerTest;
+
+import org.junit.Test;
+import org.myproject.library.library.controller.HomeController;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+public class HomeControllerTest {
+
+    @Test
+    public void testHomePage() throws Exception {
+        HomeController controller = new HomeController();
+        MockMvc mockMvc = standaloneSetup(controller).build();
+        mockMvc.perform(get("/")).andExpect(view().name("home")).andExpect(status().isOk());
+    }
+}
